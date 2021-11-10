5b62c43ff21938116f606cf9223b9e7fd1be66a6
alanmartinc <alancabot.ac@gmail.com>
2021-11-09 23:00:27 -0300

    Se agrega variables

---
+++ Curso Basico de JavaScript/variables.js
@@ -0,0 +1,14 @@
+var nombre = "Oscar"
+
+// Declarar
+var edad;
+
+// Inicializar 
+edad = 30;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
