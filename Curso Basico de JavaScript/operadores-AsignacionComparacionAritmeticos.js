cbd3802e620180ef417d88b31b3bab9813a05c80
alanmartinc <alancabot.ac@gmail.com>
2021-11-10 00:40:32 -0300

    Se agrega ejercicios operadores de asignacion, comparacion y aritmeticos

---
+++ Curso Basico de JavaScript/operadores-AsignacionComparacionAritmeticos.js
@@ -0,0 +1,47 @@
+/* Operadores aritméticos */
+3 + 2 
+// 5
+
+50 - 10
+// 40
+
+10 * 20
+// 200
+
+20 / 2
+// 10
+
+"Diego " + "De Granda"
+// "Diego De Granda"
+
+/*------------------------------------------------*/
+
+/* Operadores lógicos */
+!false
+// true
+
+/*------------------------------------------------*/
+
+/* Operadores de asignación */
+var a = 1;
+
+/*------------------------------------------------*/
+
+/* Operadores de comparación */
+3 == "3";
+// true
+
+3 === "3";
+// false
+
+5 < 3
+// false
+
+5 > 3
+// true
+
+5 <= 6
+// true
+
+5 >= 6
+// false
