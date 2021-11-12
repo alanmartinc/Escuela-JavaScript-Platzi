bdb84b5aeaf926347c4ee8483fd16687f3befb4e
alanmartinc <alancabot.ac@gmail.com>
2021-11-11 22:24:42 -0300

    Se agrega ejercicios switch

---
+++ Curso Basico de JavaScript/switch.js
@@ -0,0 +1,16 @@
+// Forma de validar por casos
+
+var numero = 1;
+switch (numero) {
+    case 1:
+        console.log("Soy un 1!");
+        break;
+    case 10:
+        console.log("Soy un 10!");
+        break;
+    case 100:
+        console.log("Soy un 100!");
+        break;
+    default:
+        console.log("No soy nada");
+}
