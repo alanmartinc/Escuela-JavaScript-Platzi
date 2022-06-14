b2f9edf216627ea3c7918d1218610e42a6570ccf
alanmartinc <alancabot.ac@gmail.com>
2022-06-14 01:27:02 -0300

    Se agrega ejercicios recorriendo Arrays con .find(), .forEach() y .some()

--- Curso Basico de JavaScript/metodosArrays.js
+++ Curso Basico de JavaScript/metodosArrays.js
@@ -32,3 +32,24 @@ var nombreArticulos = articulos.map(function (articulo) {
 nombreArticulos;
 
 /*------------------------------------------------*/
+
+/*Recorriendo Arrays con .find(), .forEach() y .some() */
+
+// Find: Nos devuelve el primer elemento que cumpla con la condición
+var articuloEncontrado = articulos.find(function (articulo) {
+  return articulo.nombre === "Teclado";
+});
+
+articuloEncontrado;
+
+// ForEach: Recorre el array y ejecuta una función
+articulos.forEach(function (articulo) {
+  console.log(articulo.nombre);
+});
+
+// Some: Valida si algo es verdadero o falso
+var articulosBaratos = articulos.some(function (articulo) {
+  return articulo.precio < 200;
+});
+
+articulosBaratos;
