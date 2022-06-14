99982effa9dd3512a99128bb8711508b2105ae4b
alanmartinc <alancabot.ac@gmail.com>
2022-06-14 00:54:54 -0300

    Se agrega ejercicios Objetos función constructora

--- Curso Basico de JavaScript/objects.js
+++ Curso Basico de JavaScript/objects.js
@@ -1,3 +1,4 @@
+/* Objetos */
 var miAuto = {
   marca: "Ford",
   modelo: "Mustang",
@@ -13,3 +14,21 @@ miAuto.marca;
 
 // Llamar a la funcion dentro del objeto
 miAuto.detalleDelAuto();
+
+/*------------------------------------------------*/
+
+/* Objetos Función constructora */
+function auto(marca, modelo, anio, color) {
+  this.marca = marca;
+  this.modelo = modelo;
+  this.anio = anio;
+  this.color = color;
+}
+
+// Crear un objeto
+// New es una palabra reservada, va a generar una nueva instancia de nuestra funcion constructora
+// Una instancia es generar un objeto que deriva de otro objeto
+var autoNuevo = new auto("Ford", "Mustang", "2015", "negro");
+
+// Llamar al nuevo objeto
+autoNuevo;
