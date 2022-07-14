cce15afefa0703229b9ddea01af77bd14bd0284c
alanmartinc <alancabot.ac@gmail.com>
2022-07-14 17:55:09 -0300

    Se agrega ejercicios Let y const, multilinea, spread operator y desestructuración

--- Curso de ECMAScript 6+/src/es6/index.js
+++ Curso de ECMAScript 6+/src/es6/index.js
@@ -9,8 +9,9 @@ function newFunction(name, age, country) {
 function newFunction2(name, age, country) {
   console.log(name, age, country);
 }
-
 newFunction2("Alan", 25, "AR");
+
+// --------------------------------------------------------
 
 let hello = "Hello";
 let world = "World";
@@ -20,3 +21,56 @@ console.log(epicPhrase);
 // es6
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+// --------------------------------------------------------
+
+let lorem =
+  "Qui consequatur. Commodi. Ipsum vel duis yet minima \n" +
+  "otra frase epica que necesitamos.";
+console.log(lorem);
+
+// es6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica
+`;
+console.log(lorem2);
+
+// --------------------------------------------------------
+
+let person = {
+  name: "Alan",
+  age: 25,
+  country: "AR",
+};
+console.log(person.name, person.age, person.country);
+
+// es6
+let person2 = {
+  name: "Alan",
+  age: 25,
+  country: "AR",
+};
+
+let { name, age, country } = person2;
+console.log(name, age, country);
+
+// --------------------------------------------------------
+
+let team1 = ["Oscar", "Julian", "Ricardo"];
+let team2 = ["Valeria", "Yesica", "Camila"];
+let education = [
+  "David",
+  "Oscar",
+  "Julian",
+  "Ricardo",
+  "Valeria",
+  "Yesica",
+  "Camila",
+];
+console.log(education);
+
+// es6
+let team3 = ["Oscar", "Julian", "Ricardo"];
+let team4 = ["Valeria", "Yesica", "Camila"];
+let education2 = ["David", ...team3, ...team4];
+console.log(education2);
