c36800386cacf288bf17bd92be727b90579b5c42
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2022-10-23 18:45:42 -0300

    Se agrega ejercicios addEventListener

--- Curso Practico de JavaScript/script.js
+++ Curso Practico de JavaScript/script.js
@@ -1,10 +1,11 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#calculo1');
 const input1 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
-
-function btnOnClick() {
+btn.addEventListener('click', sumarInputValues)
+function sumarInputValues(event) {
   const sumaInputs = input1.value + input2.value;
-  pResult.innerText = "Resultado: " + sumaInputs;
+  pResult.innerText="Resultado: " + sumaInputs;
 }
