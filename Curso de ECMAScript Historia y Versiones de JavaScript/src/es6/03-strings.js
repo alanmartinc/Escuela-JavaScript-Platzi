1093b412a6027063bcbfe4459172eead5af70b08
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2022-10-27 03:15:08 -0300

    Se agrega ejercicios strings

---
+++ Curso de ECMAScript Historia y Versiones de JavaScript/src/es6/03-strings.js
@@ -0,0 +1,14 @@
+let hello = "hello";
+let world = "world";
+let epicPhrase = hello + " " + world + "!";
+console.log(epicPhrase);
+
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2)
+
+let loren = "esto es un string \n" + "esto es otra linea";
+
+let loren2 = `Esta es una frase epica la continuacion de esa frase epica.`;
+
+console.log(loren);
+console.log(loren2);
