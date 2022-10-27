35c18d3e6e0422590dac2c52ae9c74028bc62067
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2022-10-27 03:18:03 -0300

    Se agrega ejercicios asignación de desestructuración

---
+++ Curso de ECMAScript Historia y Versiones de JavaScript/src/es6/05-rest-spread.js
@@ -0,0 +1,11 @@
+let fruits = ["Apple", "Banana"];
+let [a, b] = fruits;
+
+console.log(a, b);
+console.log(a, fruits[1]);
+
+let user = { username: "Alexa", age: 34 };
+let { username, age } = user;
+
+console.log(username, age);
+console.log(username, user.age);
