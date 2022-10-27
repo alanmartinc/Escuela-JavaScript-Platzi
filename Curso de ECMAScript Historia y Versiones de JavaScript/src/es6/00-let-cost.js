ed4c3d2d4a4104ad2fb9a9566cf7758189ff15a7
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2022-10-27 03:13:39 -0300

    Se agrega ejercicios let y const, y arrow functions

---
+++ Curso de ECMAScript Historia y Versiones de JavaScript/src/es6/00-let-cost.js
@@ -0,0 +1,24 @@
+var lastName = "David";
+lastName = "Oscar";
+console.log(lastName);
+
+let fruit = "apple";
+fruit = "kiwi";
+console.log(fruit);
+
+const animal = "dog";
+animal = "cat";
+console.log(animal);
+
+const fruits = () => {
+    if(true) {
+        var fruit1 = "Apple"
+        let fruit2 = "kiwi";
+        const fruit3 = "banana";
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+}
+
+fruits();
