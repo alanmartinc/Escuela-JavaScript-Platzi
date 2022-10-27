445766e22d6d8f344f03220f47c4e51df79e7131
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2022-10-27 04:39:22 -0300

    Se agrega ejercicios Promise.finally

---
+++ Curso de ECMAScript Historia y Versiones de JavaScript/src/es9/02-finally.js
@@ -0,0 +1,14 @@
+const anotherFuncion = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve("hey!!");
+        } else {
+            reject ( "whooooops!");
+        }
+    })
+}
+
+anotherFuncion()
+.then(response => console.log(response))
+.catch(err => console.log(err))
+.finally(() => console.log("finally"));
